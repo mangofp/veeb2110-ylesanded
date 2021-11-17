82f16cd74b6f6e818b026e3ec40a59eb1d7f22ea
Andres Järviste <andres.jarviste@wbscodingschool.com>
2021-11-17 12:34:35 +0200

    ylesanded

--- index.js
+++ index.js
@@ -2,9 +2,37 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function naitaTest(req, res) {
+  res.render('pages/test.ejs')
+}
+
+let ylesanded = [
+  {
+    kirjeldus: "Jaluta koera",
+    tehtud: true
+  },
+  {
+    kirjeldus: "Sööda kassi",
+    tehtud: false
+  },
+  {
+    kirjeldus: "Hommikusöök",
+    tehtud: false
+  },
+  {
+    kirjeldus: "Jaluta tööle",
+    tehtud: false
+  }
+]
+
+function naitaYlesandeid(req, res) {
+  res.render('pages/ylesanded.ejs', {ylesanded: ylesanded})
+}
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'avalik')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', naitaYlesandeid)
+  .get('/proov', naitaTest)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
